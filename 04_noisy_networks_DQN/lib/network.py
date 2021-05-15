ea93e51ed781afaee37432dfa2735871772d1388
rohan1198 <rohan1198@gmail.com>
2021-05-15 08:58:09 +0530

    Add files via upload

---
+++ 04_noisy_networks_DQN/lib/network.py
@@ -0,0 +1,99 @@
+import numpy as np
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class NoisyLinear(nn.Module):
+    def __init__(self, input_dim, output_dim, std_init = 0.5, bias = True):
+        super(NoisyLinear, self).__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.std_init = std_init
+        self.bias = bias
+
+        self.weight_mu = nn.Parameter(torch.FloatTensor(self.output_dim, self.input_dim))
+        self.weight_sigma = nn.Parameter(torch.FloatTensor(self.output_dim, self.input_dim))
+        self.register_buffer("weight_epsilon", torch.FloatTensor(self.output_dim, self.input_dim))
+
+        if bias:
+            self.bias_mu = nn.Parameter(torch.FloatTensor(self.output_dim))
+            self.bias_sigma = nn.Parameter(torch.FloatTensor(self.output_dim))
+            self.register_buffer("bias_epsilon", torch.FloatTensor(self.output_dim))
+
+        self.reset_parameter()
+        self.reset_noise()
+
+    
+    def reset_parameter(self):
+        mu_range = 1 / np.sqrt(self.input_dim)
+
+        self.weight_mu.detach().uniform_((-1)*mu_range, mu_range)
+        self.bias_mu.detach().uniform_((-1)*mu_range, mu_range)
+
+        self.weight_sigma.detach().fill_(self.std_init / np.sqrt(self.input_dim))
+        self.bias_sigma.detach().fill_(self.std_init / np.sqrt(self.output_dim))
+
+
+    def scale_noise(self, size):
+        noise = torch.randn(size)
+        noise = noise.sign().mul(noise.abs().sqrt())
+        return noise
+
+    
+    def reset_noise(self):
+        epsilon_in = self.scale_noise(self.input_dim)
+        epsilon_out = self.scale_noise(self.output_dim)
+
+        self.weight_epsilon.copy_(epsilon_out.ger(epsilon_in))
+        self.bias_epsilon.copy_(self.scale_noise(self.output_dim))
+
+
+    def forward(self, input):
+        weight = self.weight_mu + self.weight_sigma.mul(self.weight_epsilon)
+        bias = self.bias
+        
+        if bias is not None:
+            bias = self.bias_mu + self.bias_sigma.mul(self.bias_epsilon)
+        
+        return F.linear(input, weight, bias)
+
+
+
+
+
+class NoisyDQN(nn.Module):
+    def __init__(self, input_dims, n_actions):
+        super(NoisyDQN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(input_dims[0], 32, kernel_size = 8, stride = 4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 4, stride = 2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size = 3, stride = 1)
+
+        conv_out = self.get_conv_out(input_dims)
+
+        self.noisy1 = NoisyLinear(conv_out, 512)
+        self.noisy2 = NoisyLinear(512, n_actions)
+
+
+    def get_conv_out(self, input_dims):
+        dims = torch.zeros(1, *input_dims)
+        dims = self.conv1(dims)
+        dims = self.conv2(dims)
+        dims = self.conv3(dims)
+        return int(np.prod(dims.size()))
+
+
+    def forward(self, dims):
+        x = F.relu(self.conv1(dims))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        out = x.view(x.size(0), -1)
+
+        x = F.relu(self.noisy1(out))
+        x = self.noisy2(x)
+        return x
---
+++ 04_noisy_networks_DQN/main.py
@@ -0,0 +1,92 @@
+import numpy as np
+import time
+import argparse
+import collections
+
+import torch
+import torch.optim as optim
+
+from tensorboardX import SummaryWriter
+from lib import agent, common, experience, network, wrappers
+
+
+
+if __name__ == "__main__":
+    params = common.HYPERPARAMS["pong"]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default = False, action = "store_true", help = "Enable CUDA")
+    parser.add_argument("--env", default = params["env_name"], help = "Name of the Environment")
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+    if device == torch.device("cuda"):
+        print(torch.cuda.get_device_properties(device))
+    else:
+        print("Running on CPU!")
+    print("\n")
+
+    env = wrappers.make_env(args.env)
+
+    net = network.NoisyDQN(env.observation_space.shape, env.action_space.n).to(device)
+    tgt_net = network.NoisyDQN(env.observation_space.shape, env.action_space.n).to(device)
+
+    writer = SummaryWriter(comment = "-" + params["run_name"])
+    print(net)
+
+    buffer = experience.ReplayBuffer(capacity = params["replay_size"])
+    agent = agent.Agent(env, buffer)
+    epsilon = params["epsilon_start"]
+
+    optimizer = optim.Adam(net.parameters(), lr = params["learning_rate"])
+    total_rewards = []
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+    best_mean_reward = None
+
+    while True:
+        frame_idx += 1
+        epsilon = max(params["epsilon_final"], params["epsilon_start"] - frame_idx / params["epsilon_frames"])
+
+        reward = agent.play_step(net, epsilon, device = device)
+
+        if reward is not None:
+            total_rewards.append(reward)
+            speed = (frame_idx - ts_frame) / (time.time() - ts)
+            ts_frame = frame_idx
+            ts = time.time()
+            mean_reward = np.mean(total_rewards[-100:])
+
+            print(f"{frame_idx} frames | {len(total_rewards)} games | {reward:.3f} reward | {mean_reward:.3f} mean reward | {epsilon:.2f} epsilon | {speed:.2f} frames/sec")
+
+            writer.add_scalar("speed", speed, frame_idx)
+            writer.add_scalar("reward_100", mean_reward, frame_idx)
+            writer.add_scalar("reward", reward, frame_idx)
+
+            if best_mean_reward is None or best_mean_reward < mean_reward:
+                torch.save(net.state_dict(), args.env + "-best.dat")
+
+                if best_mean_reward is not None:
+                    print(f"Best mean reward updated {best_mean_reward:.3f} -> {mean_reward:.3f} model saved!")
+                
+                best_mean_reward = mean_reward
+            
+            if mean_reward > params["stop_reward"]:
+                print(f"Solved in {frame_idx} frames!")
+                break
+        
+        if len(buffer) < params["replay_initial"]:
+            continue
+            
+        if frame_idx % params["target_net_sync"] == 0:
+            tgt_net.load_state_dict(net.state_dict())
+        
+        optimizer.zero_grad()
+        batch = buffer.sample(params["batch_size"])
+
+        loss_t = common.calc_loss(batch, net, tgt_net, params["gamma"], device = device)
+        loss_t.backward()
+        optimizer.step()
+    
+    writer.close()
