3dd12346a677179251243d06ef1d0c16eaa43b8b
rohan1198 <rohan1198@gmail.com>
2021-05-15 09:01:25 +0530

    Add files via upload

---
+++ 06_dueling_DQN/lib/common.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+
+HYPERPARAMS = {
+    'pong': {
+        'env_name':         "PongNoFrameskip-v4",
+        'stop_reward':      18.0,
+        'run_name':         'pong',
+        'replay_size':      90000,
+        'replay_initial':   10000,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10**5,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.02,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       32
+    },
+    'breakout-small': {
+        'env_name':         "BreakoutNoFrameskip-v4",
+        'stop_reward':      100.0,
+        'run_name':         'breakout-small',
+        'replay_size':      1*10 ** 5,
+        'replay_initial':   20000,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10 ** 6,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.1,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       64
+    },
+    'breakout': {
+        'env_name':         "BreakoutNoFrameskip-v4",
+        'stop_reward':      500.0,
+        'run_name':         'breakout',
+        'replay_size':      10 ** 6,
+        'replay_initial':   50000,
+        'target_net_sync':  10000,
+        'epsilon_frames':   10 ** 6,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.1,
+        'learning_rate':    0.00025,
+        'gamma':            0.99,
+        'batch_size':       32
+    },
+    'invaders': {
+        'env_name': "SpaceInvadersNoFrameskip-v4",
+        'stop_reward': 500.0,
+        'run_name': 'breakout',
+        'replay_size': 10 ** 6,
+        'replay_initial': 50000,
+        'target_net_sync': 10000,
+        'epsilon_frames': 10 ** 6,
+        'epsilon_start': 1.0,
+        'epsilon_final': 0.1,
+        'learning_rate': 0.00025,
+        'gamma': 0.99,
+        'batch_size': 32
+    },
+}
+
+
+
+
+def calc_loss(batch, net, tgt_net, gamma, device):
+    states, actions, rewards, next_states, dones = batch
+
+    states_v = torch.tensor(states).to(device)
+    actions_v = torch.tensor(actions).to(device)
+    rewards_v = torch.tensor(rewards).to(device)
+    next_states_v = torch.tensor(next_states).to(device)
+    done_mask = torch.BoolTensor(dones).to(device)
+
+    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+
+    next_state_values = tgt_net(next_states_v).max(1)[0]
+    next_state_values[done_mask] = 0.0
+    next_state_values = next_state_values.detach()
+
+    expected_state_action_values = next_state_values * gamma + rewards_v
+
+    return nn.MSELoss()(state_action_values, expected_state_action_values)
